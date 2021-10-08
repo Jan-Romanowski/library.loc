838ddfe35ccbecc6c66912b2200def7e58188b81
Yan Ramanouski <Leonovstarget@gmail.com>
2021-10-08 10:23:10 +0300

    -

--- views/folders/folderList.php
+++ views/folders/folderList.php
@@ -2,12 +2,21 @@
 
 <div class="container p-3 gx-5 w-25">
     <table class='table table-hover'>
-        <tr class="bg-light"><td>Nazwa teczki</td>
+        <tr class="bg-light"><td>Nazwa teczki</td><td></td>
         </tr>
         <?php
         foreach ($foldersList as $foldersListItem): ?>
             <tr>
                 <td><?php echo $foldersListItem['name_folder'] ?></td>
+                <td>
+                    <button class="btn btn-outline-dark px-2 dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        Opcje
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">Zmienić nazwę</a></li>
+                        <li><a class="dropdown-item" href="#">Usunąć teczkę</a></li>
+                    </ul>
+                </td>
             </tr>
         <?php endforeach;  ?>
     </table>
