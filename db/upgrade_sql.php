0ac120e07923f7f858764f267e31baa4cdb5d88e
Yan Ramanouski <50454481+Chickennaggets@users.noreply.github.com>
2021-11-15 16:40:57 +0100

    На пол пути..

--- db/upgrade_sql.php
+++ db/upgrade_sql.php
@@ -5,3 +5,12 @@ $sqls['1.0.0'] = "ALTER TABLE `accounts`
 										CHANGE COLUMN `ac_password` `ac_password` VARCHAR(51) 
 										CHARACTER SET 'utf8mb4' NULL DEFAULT NULL";
 $sqls['1.0.1'] = "UPDATE accounts SET ac_password = MD5(ac_password)";
+
+$sqls['1.0.2'] = "CREATE TABLE IF NOT EXISTS `news` (
+  `id_news` int(11) NOT NULL,
+  `header` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `text` varchar(300) COLLATE utf8_polish_ci NOT NULL,
+  `date_news` datetime NOT NULL DEFAULT current_timestamp(),
+  `autor` text COLLATE utf8_polish_ci NOT NULL,
+      PRIMARY KEY (id_news)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci";
